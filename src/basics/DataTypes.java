51bb33d5b8e19c02ec9f0ef8d933e9c9fa1f69ac
Mandula Naresh Babu <naresh2223@gmail.com>
2019-07-01 23:02:01 -0400

    01-July-19:Naresh:Added Code to DataTypes.Java file

--- src/basics/DataTypes.java
+++ src/basics/DataTypes.java
@@ -6,6 +6,25 @@ public class DataTypes {
 		
 		System.out.println("Welcome to Core Java training");
 		System.out.println("Learn about Primitive and non primitive Data types used in Java");
+		
+		//Primitive Data Types
+		int a=10;
+		boolean b =true;
+		boolean c =false;
+		char d='a';
+		float f = 12.53f;
+		double g =123.435436;
+		byte b1=127;
+		long l=12243;
+		System.out.println("Integer value of a is : " + a);
+		System.out.println("Boolean value of b is : " + b);
+		System.out.println("Boolean value of c is : " + c);
+		System.out.println("Char value of d is : " + d);
+		System.out.println("Float value of f is : " + f);
+		System.out.println("Double value of g is : " + g);
+		System.out.println("Byte value of b1 is : " + b1);
+		System.out.println("Long value of l is : " + l);
+		
 
 	}
 
