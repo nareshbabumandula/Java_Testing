f29ee368a8aff5b316cef143df7def2a985345d3
Mandula Naresh Babu <naresh2223@gmail.com>
2019-07-01 22:31:07 -0400

    01-July-19:Naresh: DatatTypes with examples

---
+++ src/basics/DataTypes.java
@@ -0,0 +1,13 @@
+package basics;
+
+public class DataTypes {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Welcome to Core Java training");
+		System.out.println("Learn about Primitive and non primitive Data types used in Java");
+
+	}
+
+}
+	
