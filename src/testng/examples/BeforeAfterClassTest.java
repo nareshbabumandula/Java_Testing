9f3366e1b4d6a649179204fafd2e7cfd70b0acd1
Mandula Naresh Babu <naresh2223@gmail.com>
2019-08-12 01:06:33 -0400

    12-Aug-2019:Naresh: Added Java Documentation

--- src/testng/examples/BeforeAfterClassTest.java
+++ src/testng/examples/BeforeAfterClassTest.java
@@ -6,39 +6,43 @@ import org.testng.Reporter;
 import org.testng.annotations.AfterClass;
 
 public class BeforeAfterClassTest {
- 
-  @BeforeClass()
-  public void Login() 
-  {
-	  System.out.println("Executing Login method...");
-  }
-    
-  @Test
-  public void SearchProduct() 
-  {
-	  System.out.println("Executing SearchProduct method...");
-	  Reporter.log("Successfully searched the product");
-  }
-  
-  @Test(dependsOnMethods= {"AddToCart"})
-  public void Checkout() 
-  {
-	  System.out.println("Executing Checkout method...");
-	  Reporter.log("Successfully checked out the product");
-  }
-  
-  @Test(dependsOnMethods= {"SearchProduct"})
-  public void AddToCart() 
-  {
-	  System.out.println("Executing AddToCart method...");
-	  Reporter.log("Successfully added the product to the cart");
-  }
-    
 
-  @AfterClass()
-  public void Logout()
-  {
-	  System.out.println("Executing Logout method...");
-  }
+
+	/**
+	 * @author NareshBabu
+	 */
+	@BeforeClass()
+	public void Login() 
+	{
+		System.out.println("Executing Login method...");
+	}
+
+	@Test
+	public void SearchProduct() 
+	{
+		System.out.println("Executing SearchProduct method...");
+		Reporter.log("Successfully searched the product");
+	}
+
+	@Test(dependsOnMethods= {"AddToCart"})
+	public void Checkout() 
+	{
+		System.out.println("Executing Checkout method...");
+		Reporter.log("Successfully checked out the product");
+	}
+
+	@Test(dependsOnMethods= {"SearchProduct"})
+	public void AddToCart() 
+	{
+		System.out.println("Executing AddToCart method...");
+		Reporter.log("Successfully added the product to the cart");
+	}
+
+
+	@AfterClass()
+	public void Logout()
+	{
+		System.out.println("Executing Logout method...");
+	}
 
 }
